0adb7bb465bd49d6693c3a54a37f6241dfd7ed88
Garrett Brown <garbearucla@gmail.com>
2013-02-26 04:58:22 -0800

    Refactor game client serialization into its own class

--- xbmc/settings/windows/GUIWindowSettingsCategory.cpp
+++ xbmc/settings/windows/GUIWindowSettingsCategory.cpp
@@ -999,6 +999,12 @@ void CGUIWindowSettingsCategory::UpdateSettings()
       if (pControl)
         pControl->SetEnabled(CPeripheralImon::GetCountOfImonsConflictWithDInput() == 0);
     }
+    else if (strSetting.Equals("games.rewindtime"))
+    {
+      CGUIControl *pControl = (CGUIControl *)GetControl(pSettingControl->GetID());
+      if (pControl)
+        pControl->SetEnabled(g_guiSettings.GetBool("games.enablerewind"));
+    }
   }
 
   g_guiSettings.SetChanged();
