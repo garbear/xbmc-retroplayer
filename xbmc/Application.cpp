4d29cb8483dc92c43c2ab81c7250b0040a98f86b
Garrett Brown <garbearucla@gmail.com>
2013-03-20 13:14:17 -0700

    [savestates] Added new Save State Manager dialog window

--- xbmc/Application.cpp
+++ xbmc/Application.cpp
@@ -277,6 +277,7 @@
 /* Game-related include files */
 #include "cores/RetroPlayer/RetroPlayer.h"
 #include "games/windows/GUIWindowGames.h"
+#include "games/savegames/GUIDialogGameSaves.h"
 
 #ifdef HAS_PERFORMANCE_SAMPLE
 #include "utils/PerformanceSample.h"
@@ -1351,6 +1352,7 @@ bool CApplication::Initialize()
     g_windowManager.Add(new CGUIWindowStartup);
 
     g_windowManager.Add(new CGUIWindowGames);
+    g_windowManager.Add(new CGUIDialogGameSaves);
 
     /* window id's 3000 - 3100 are reserved for python */
 
@@ -3389,6 +3391,7 @@ bool CApplication::Cleanup()
     g_windowManager.Remove(WINDOW_DIALOG_VOLUME_BAR);
 
     g_windowManager.Delete(WINDOW_GAMES);
+    g_windowManager.Delete(WINDOW_DIALOG_GAME_SAVES);
 
     CAddonMgr::Get().DeInit();
 
