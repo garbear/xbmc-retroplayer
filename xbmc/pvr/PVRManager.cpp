6d0bc9873d6322b2c771b9d0146eb8334c97c2ce
Garrett Brown <garbearucla@gmail.com>
2013-09-04 21:44:28 -0700

    Refactor service starting/stopping using a callback-based interface when enabling/disabling add-ons in the add-on database

--- xbmc/pvr/PVRManager.cpp
+++ xbmc/pvr/PVRManager.cpp
@@ -184,6 +184,20 @@ void CPVRManager::OnSettingAction(const CSetting *setting)
   }
 }
 
+bool CPVRManager::AddonEnabled(ADDON::AddonPtr addon, bool bDisabled)
+{
+  // (re)start the pvr manager when enabling a pvr add-on
+  Start(true);
+  return true;
+}
+
+void CPVRManager::AddonDisabled(ADDON::AddonPtr addon)
+{
+  // restart the pvr manager when disabling a pvr add-on with the pvr manager enabled
+  if (IsStarted())
+    Start(true);
+}
+
 bool CPVRManager::IsPVRWindowActive(void) const
 {
   return g_windowManager.IsWindowActive(WINDOW_PVR) ||
@@ -436,6 +450,8 @@ void CPVRManager::Process(void)
   else
     return;
 
+  CAddonDatabase::RegisterAddonDatabaseCallback(ADDON::ADDON_PVRDLL, this);
+
   /* main loop */
   CLog::Log(LOGDEBUG, "PVRManager - %s - entering main loop", __FUNCTION__);
   g_EpgContainer.Start();
@@ -477,6 +493,8 @@ void CPVRManager::Process(void)
     else if (IsStarted() && !bRestart)
       m_triggerEvent.WaitMSec(1000);
   }
+
+  CAddonDatabase::UnregisterAddonDatabaseCallback(ADDON::ADDON_PVRDLL);
 
   if (IsStarted())
   {
