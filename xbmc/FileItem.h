b5b405963342a589d423d1ae674aa214bb99dccf
Garrett Brown <garbearucla@gmail.com>
2013-09-10 21:41:28 -0700

    Added game info tags to XBMC. This is mostly a stop-gap implementation until PyRomInfo (https://github.com/garbear/pyrominfo) is included.
    Thanks to fetzerch for fixing a PVR-related crash.

--- xbmc/FileItem.h
+++ xbmc/FileItem.h
@@ -53,6 +53,10 @@ namespace PVR
   class CPVRTimerInfoTag;
 }
 class CPictureInfoTag;
+namespace GAME_INFO
+{
+  class CGameInfoTag;
+}
 
 class CAlbum;
 class CArtist;
@@ -151,6 +155,7 @@ public:
    */
   bool IsAudio() const;
 
+  bool IsGame() const;
   bool IsKaraoke() const;
   bool IsCUESheet() const;
   bool IsInternetStream(const bool bStrictCheck = false) const;
@@ -305,6 +310,18 @@ public:
     return m_pictureInfoTag;
   }
 
+  inline bool HasGameInfoTag() const
+  {
+    return m_gameInfoTag != NULL;
+  }
+
+  GAME_INFO::CGameInfoTag* GetGameInfoTag();
+
+  inline const GAME_INFO::CGameInfoTag* GetGameInfoTag() const
+  {
+    return m_gameInfoTag;
+  }
+
   CPictureInfoTag* GetPictureInfoTag();
 
   /*!
@@ -369,6 +386,7 @@ public:
   CStdString FindTrailer() const;
 
   virtual bool LoadMusicTag();
+  virtual bool LoadGameTag();
 
   /* Returns the content type of this item if known */
   const CStdString& GetMimeType() const { return m_mimetype; }
@@ -451,6 +469,7 @@ private:
   PVR::CPVRRecording* m_pvrRecordingInfoTag;
   PVR::CPVRTimerInfoTag * m_pvrTimerInfoTag;
   CPictureInfoTag* m_pictureInfoTag;
+  GAME_INFO::CGameInfoTag* m_gameInfoTag;
   bool m_bIsAlbum;
 };
 
