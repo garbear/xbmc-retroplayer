d8aa8493010f6348e69defc1c86cff22da2c9e7a
Garrett Brown <garbearucla@gmail.com>
2013-09-10 21:41:28 -0700

    [retroplayer] - (0006) Game info tags
    This is mostly a stop-gap implementation until PyRomInfo
    (https://github.com/garbear/pyrominfo) is included.
    
    Thanks to fetzerch for fixing a PVR-related crash.

--- xbmc/FileItem.h
+++ xbmc/FileItem.h
@@ -56,6 +56,10 @@ namespace PVR
   typedef std::shared_ptr<PVR::CPVRTimerInfoTag> CPVRTimerInfoTagPtr;
 }
 class CPictureInfoTag;
+namespace GAME
+{
+  class CGameInfoTag;
+}
 
 class CAlbum;
 class CArtist;
@@ -170,6 +174,7 @@ public:
    */
   bool IsAudio() const;
 
+  bool IsGame() const;
   bool IsKaraoke() const;
   bool IsCUESheet() const;
   bool IsInternetStream(const bool bStrictCheck = false) const;
@@ -329,6 +334,18 @@ public:
     return m_pictureInfoTag;
   }
 
+  inline bool HasGameInfoTag() const
+  {
+    return m_gameInfoTag != NULL;
+  }
+
+  GAME::CGameInfoTag* GetGameInfoTag();
+
+  inline const GAME::CGameInfoTag* GetGameInfoTag() const
+  {
+    return m_gameInfoTag;
+  }
+
   CPictureInfoTag* GetPictureInfoTag();
 
   /*!
@@ -399,6 +416,7 @@ public:
   std::string FindTrailer() const;
 
   virtual bool LoadMusicTag();
+  virtual bool LoadGameTag();
 
   /* Returns the content type of this item if known */
   const std::string& GetMimeType() const { return m_mimetype; }
@@ -491,6 +509,7 @@ private:
   PVR::CPVRRecordingPtr m_pvrRecordingInfoTag;
   PVR::CPVRTimerInfoTagPtr m_pvrTimerInfoTag;
   CPictureInfoTag* m_pictureInfoTag;
+  GAME::CGameInfoTag* m_gameInfoTag;
   bool m_bIsAlbum;
 
   CCueDocumentPtr m_cueDocument;
