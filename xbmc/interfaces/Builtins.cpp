xbmc/xbmc

[retroplayer] Added RetroPlayer, a new player core that enables game clients to play video and audio.
Thanks to elpendor for RGB565 support, poisson for RAII improvements and ChrisMyhre for catching a compile error.

xbmc/interfaces/Builtins.cpp
@@ -62,6 +62,7 @@
 #include "URL.h"
 #include "music/MusicDatabase.h"
 #include "cores/IPlayer.h"
+#include "games/tags/GameInfoTag.h"
 
 #include "filesystem/PluginDirectory.h"
 #ifdef HAS_FILESYSTEM_RAR
@@ -578,6 +579,12 @@ int CBuiltins::Execute(const CStdString& execString)
           // Pass the script name (params[0]) and all the parameters
           // (params[1] ... params[x]) separated by a comma to RunScript
           cmd.Format("RunScript(%s)", StringUtils::JoinString(params, ","));
+        else if (addon->Type() == ADDON_GAMEDLL && params.size() >= 2)
+        {
+          CFileItem item(params[1], false);
+          item.SetProperty("gameclient", params[0]);
+          return g_application.PlayMedia(item);
+        }
 
         return Execute(cmd);
       }
@@ -632,6 +639,16 @@ int CBuiltins::Execute(const CStdString& execString)
       else if (params[i].Left(11).Equals("playoffset=")) {
         playOffset = atoi(params[i].Mid(11)) - 1;
         item.SetProperty("playlist_starting_track", playOffset);
+      }
+      else if (params[i].Left(9).Equals("platform="))
+      {
+        // A game platform was specified, record the request for when we choose a game client
+        item.GetGameInfoTag()->SetPlatform(params[i].Mid(9));
+      }
+      else if (params[i].Left(11).Equals("gameclient="))
+      {
+        // A game client ID was specified
+        item.SetProperty("gameclient", params[i].Mid(11));
       }
     }
 

garbear/xbmc-retroplayer

xbmc/interfaces/Builtins.cpp
@@ -65,6 +65,7 @@
 #include "URL.h"
 #include "music/MusicDatabase.h"
 #include "cores/IPlayer.h"
+#include "games/tags/GameInfoTag.h"
 
 #include "filesystem/PluginDirectory.h"
 #ifdef HAS_FILESYSTEM_RAR
@@ -593,6 +594,12 @@ int CBuiltins::Execute(const CStdString& execString)
           // Pass the script name (params[0]) and all the parameters
           // (params[1] ... params[x]) separated by a comma to RunScript
           cmd = StringUtils::Format("RunScript(%s)", StringUtils::JoinString(params, ",").c_str());
+        else if (addon->Type() == ADDON_GAMEDLL && params.size() >= 2)
+        {
+          CFileItem item(params[1], false);
+          item.SetProperty("gameclient", params[0]);
+          return g_application.PlayMedia(item);
+        }
 
         return Execute(cmd);
       }
@@ -660,6 +667,16 @@ int CBuiltins::Execute(const CStdString& execString)
       else if (StringUtils::StartsWithNoCase(params[i], "playoffset=")) {
         playOffset = atoi(params[i].substr(11).c_str()) - 1;
         item.SetProperty("playlist_starting_track", playOffset);
+      }
+      else if (StringUtils::StartsWithNoCase(params[i], "platform="))
+      {
+        // A game platform was specified, record the request for when we choose a game client
+        item.GetGameInfoTag()->SetPlatform(params[i].substr(9));
+      }
+      else if (StringUtils::StartsWithNoCase(params[i], "gameclient="))
+      {
+        // A game client ID was specified
+        item.SetProperty("gameclient", params[i].substr(11));
       }
     }
 