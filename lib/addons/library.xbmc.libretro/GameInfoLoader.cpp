23393ad3e86e1e8b293025c789c4b4a83ad6ef10
Garrett Brown <garbearucla@gmail.com>
2014-04-15 23:31:07 -0700

    Fix compile of library.xbmc.libretro on OSX

--- lib/addons/library.xbmc.libretro/GameInfoLoader.cpp
+++ lib/addons/library.xbmc.libretro/GameInfoLoader.cpp
@@ -19,6 +19,16 @@
  */
 
 #include "GameInfoLoader.h"
+
+// TODO: This must #defined before libXBMC_addon.h to fix compile on OS X
+#include <sys/stat.h>
+#if defined(TARGET_DARWIN) || defined(TARGET_FREEBSD)
+  #define stat64 stat
+  #define __stat64 stat
+#else
+  #define __stat64 stat64
+#endif
+
 #include "libXBMC_addon.h"
 
 using namespace ADDON;
