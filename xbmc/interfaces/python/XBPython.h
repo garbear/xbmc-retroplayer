86404e7116b5d4ba9780ae2f17407d4c9a97957d
Achim Turan <mail@achim-turan.de>
2015-07-22 19:49:22 +0200

    [interfaces] - (0001) XBPython extension to allow binary addon calls

--- xbmc/interfaces/python/XBPython.h
+++ xbmc/interfaces/python/XBPython.h
@@ -26,17 +26,29 @@
 #include "threads/Thread.h"
 #include "interfaces/IAnnouncer.h"
 #include "interfaces/generic/ILanguageInvocationHandler.h"
+#include "addons/IAddon.h"
+#include <Python.h>
 
 #include <memory>
 #include <vector>
 
 class CPythonInvoker;
 
-typedef struct {
+typedef struct PyElem
+{
   int id;
   bool bDone;
-  CPythonInvoker* pyThread;
-}PyElem;
+  CPythonInvoker *pyThread;
+  PyThreadState  *addonPythonState; // this pointer is only used by binary add-ons to controll a full python interpreter
+
+  PyElem()
+  {
+    id = 0;
+    bDone = false;
+    pyThread = NULL;
+    addonPythonState = NULL;
+  };
+} PyElem;
 
 class LibraryLoader;
 
@@ -106,7 +118,21 @@ public:
   void UnregisterExtensionLib(LibraryLoader *pLib);
   void UnloadExtensionLibs();
 
+  // ------------------------------------------------------------------
+  //               Python Addon Interpreter methods
+  // ------------------------------------------------------------------
+  /**
+   * This method tries to get a new Python interpreter thread state, which can be used by dynamic linking to Python.dll.
+   * @return a new ScriptId.
+   */
+  int  GetAddonInterpreter();
+  bool DestroyAddonInterpreter(int Id);
+  bool ActiveAddonInterpreter(int Id);
+  bool DeactiveAddonInterpreter(int Id);
+  bool FinishAddonInterpreter(int Id);
+
 private:
+  bool InitInterpreter();
   void Finalize();
 
   CCriticalSection    m_critSection;
